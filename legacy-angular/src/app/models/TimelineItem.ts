1ee59e29d70e13b0836e5cbc7799fd6f45334239
Andrea Galliani <andrea.galliani.29@gmail.com>
2026-03-03 14:33:23 +0100

    chore: remove legacy Angular application and associated development scripts.

--- legacy-angular/src/app/models/TimelineItem.ts
+++
@@ -1,6 +0,0 @@
-export interface TimelineItem {
-    start: string; // Format: "MM/YYYY" or "YYYY"
-    end: string | null; // Format: "MM/YYYY" or "YYYY" or null
-    label: string;
-    category: string;
-}
